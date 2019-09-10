a094d8b13a7a96dc3af5c1c05cfaaffa79cc6ff3
Igor Tkachev <tkacheviscoder@gmail.com>
2019-09-10 23:11:32 +0300

    Updating our app to redirect to home if user is signed in

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 import SignIn from '../../components/sign-in/SignIn'
 import SignUp from '../../components/sign-up/SignUp'
 
 import './login.scss'
 
-const Login = () => {
+const Login = ({ currentUser }) => {
 	return (
 		<div className="sign-in-and-sign-up">
+			{currentUser && <Redirect to="/" />}
 			<SignIn />
 			<SignUp />
 		</div>
 	)
 }
 
-export default Login
+export default connect(state => ({ currentUser: state.user.currentUser }))(
+	Login
+)
