8cba57b1b53314e3edc8647bc9d549079b152a0f
Igor Tkachev <tkacheviscoder@gmail.com>
2019-10-10 17:37:09 +0300

    Added route for payment processing

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
 process.env.NODE_ENV !== 'production' && require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,4 +26,22 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, err => {
 	if (err) throw err;
 	console.log(`App listening on port ${port}!`);
+});
+
+app.post('/payment', (req, res) => {
+	const { token, amount } = req.body;
+
+	const body = {
+		source: token.id,
+		amount,
+		currency: 'rub',
+	};
+
+	stripe.charges.create(body, (stripeErr, stripeRes) => {
+		if (stripeErr) {
+			res.status(500).send({ error: stripeErr });
+		} else {
+			res.status(200).send({ success: stripeRes });
+		}
+	});
 });
